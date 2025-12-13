852ae561252d49858b664214021b17d85950ac67
DevasNB <joaodevesa2019@gmail.com>
2025-12-13 22:16:10 +0000

    feat: add profile page and navigation from home

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
+// bootstrap main.ts
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { App } from './app/app';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)],
+}).catch(err => console.error(err));
